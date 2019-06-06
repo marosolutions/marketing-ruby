fb9b9bd0cc6fc9ecc9b435b9db9e366d07ac1603
ashutears <aacharya@ap-logic.com>
2019-06-06 07:00:49 +0545

    puts documentation comments on each methods

--- lib/maropost_api/reports.rb
+++ lib/maropost_api/reports.rb
@@ -1,15 +1,21 @@
 module MaropostApi
 
   ##
-  # It contains methods that get reports based on provided parameters.
+  # Contains methods that get reports based on provided parameters.
   # The method names themselves reveal the type of reports they are getting.
   class Reports
-    
+    ##
+    # Creates a new instance of Reports class.
+    # Takes +account+ and +api_key+ as parameters where 
+    # @param account [Integer] is authentic user account id (Integer) provided by maropost.com
+    # @param api_key [String] is the auth token (String) that is validated on the server to authenticate the user
     def initialize(account = ENV["ACCOUNT"], api_key = ENV["API_KEY"])
       MaropostApi.instance_variable_set(:@api_key, api_key)
       MaropostApi.instance_variable_set(:@account, account)
     end
     
+    ##
+    # gets all the reports grouped by +page+ (Integer)
     def get(page)
       full_path = full_resource_path
       query_params = MaropostApi.set_query_params({'page' => page})
@@ -17,6 +23,8 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
   
+    ##
+    # gets a report defined by its unique +id+ (Integer)
     def get_report(id)
       full_path = full_resource_path "/#{id}"
       query_params = MaropostApi.set_query_params
@@ -24,6 +32,16 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # gets all open reports described by 
+    # @param page [Integer] Integer that tells the service what page of report to retrieve
+    # @param fields [Array] that contains the name of the fields of the contact to retrieve
+    # @param from [Date] formatted string to add a constriant to our result
+    # @param to [Date] Part of date range of +from+. Determines the result retrieved is in between this range of date
+    # @param unique [Boolean] that indicates whether to get unique reports or not
+    # @param email [String] email to be equated while querying for reports
+    # @param uid [String] carrying unique identifier for the report.
+    # @param per [Integer] determining how many records to get for each page
     def get_opens(page:, fields: [], from: nil, to: nil, unique: nil, email: nil, uid: nil, per: nil)
       full_path = full_resource_path "/opens"
       params = {}
@@ -35,6 +53,16 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # gets all click reports described by 
+    # @param page [Integer] that tells the service what page of report to retrieve
+    # @param fields [Array] that contains the name of the fields of the contact to retrieve
+    # @param from [Date] formatted string to add a constriant to our result
+    # @param to [Part] of date range of +from+. Determines the result retrieved is in between this range of date
+    # @param unique [Boolean] that indicates whether to get unique reports or not
+    # @param email [String] email to be equated while querying for reports
+    # @param uid [String] carrying unique identifier for the report.
+    # @param per [Integer] determining how many records to get for each page
     def get_clicks(page:, fields: [], from: nil, to: nil, unique: nil, email: nil, uid: nil, per: nil)
       full_path = full_resource_path "/clicks"
       params = {}
@@ -46,6 +74,17 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # gets all bounce reports described by 
+    # @param page [Integer] that tells the service what page of report to retrieve
+    # @param fields [Array] that contains the name of the fields of the contact to retrieve
+    # @param from [Date] formatted string to add a constriant to our result
+    # @param to [Part] of date range of +from+. Determines the result retrieved is in between this range of date
+    # @param unique [Boolean] that indicates whether to get unique reports or not
+    # @param email [String] email to be equated while querying for reports
+    # @param uid [String] carrying unique identifier for the report.
+    # @param per [Integer] determining how many records to get for each page
+    # @param type [String] Set of values determining "soft" or "hard" bounces
     def get_bounces(page:, fields: [], from: nil, to: nil, unique: nil, email: nil, uid: nil, per: nil, type: nil)
       full_path = full_resource_path "/bounces"
       params = {}
@@ -57,6 +96,16 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # gets all unsubscribe reports described by 
+    # @param page: [Integer] that tells the service what page of report to retrieve
+    # @param fields: [Array] that contains the name of the fields of the contact to retrieve
+    # @param from: [Date] formatted string to add a constriant to our result
+    # @param to: [Date] part of date range of +from+. Determines the result retrieved is in between this range of date
+    # @param unique: [Boolean] that indicates whether to get unique reports or not
+    # @param email: [String] email to be equated while querying for reports
+    # @param uid: [String] carrying unique identifier for the report.
+    # @param per: [Integer] determining how many records to get for each page
     def get_unsubscribes(page:, fields: [], from: nil, to: nil, unique: nil, email: nil, uid: nil, per: nil)
       full_path = full_resource_path "/unsubscribes"
       params = {}
@@ -68,6 +117,16 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # gets all complaint reports described by 
+    # @param page [Integer] that tells the service what page of report to retrieve
+    # @param fields [Array] that contains the name of the fields of the contact to retrieve
+    # @param from [Date] formatted string to add a constriant to our result
+    # @param to [Date] part of date range of +from+. Determines the result retrieved is in between this range of date
+    # @param unique [Boolean] that indicates whether to get unique reports or not
+    # @param email [String] email to be equated while querying for reports
+    # @param uid [String] carrying unique identifier for the report.
+    # @param per [Integer] determining how many records to get for each page
     def get_complaints(page:, fields: [], from: nil, to: nil, unique: nil, email: nil, uid: nil, per: nil)
       full_path = full_resource_path "/unsubscribes"
       params = {}
@@ -79,6 +138,13 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # gets all ab reports described by 
+    # @param page [Integer] that tells the service what page of report to retrieve
+    # @param fields [Array] that contains the name of the fields of the contact to retrieve
+    # @param from [Date] formatted string to add a constriant to our result
+    # @param to [Date] part of date range of +from+. Determines the result retrieved is in between this range of date
+    # @param per [Integer] determining how many records to get for each page
     def get_ab_reports(name:, page:, from: nil, to: nil, per: nil)
       full_path = full_resource_path('', "ab_reports")
       params = {}
@@ -90,6 +156,9 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ## 
+    # gets all journey reports for the provided +page+
+    # @param page [Integer]
     def get_journeys(page)
       full_path = full_resource_path '/journeys'
       query_params = MaropostApi.set_query_params({:page => page})
