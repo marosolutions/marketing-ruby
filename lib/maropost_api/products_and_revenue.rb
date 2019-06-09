950f635e3de8a8e90e68297a6dc838876d8697ad
ashutears <aacharya@ap-logic.com>
2019-06-10 03:13:26 +0545

    puts doc comments or products_and_revenue and relation_tables clases

--- lib/maropost_api/products_and_revenue.rb
+++ lib/maropost_api/products_and_revenue.rb
@@ -1,14 +1,22 @@
 require 'maropost_api/custom_types/order_item'
 
 module MaropostApi
-  
+  ##
+  # Contains methods that will create, update, read and delete the services
+  # provided in the Product and Revenue Api
   class ProductsAndRevenue
-    
+    ##
+    # Creates a new instance of Reports class.
+    # @param account [Integer] is authentic user account id (Integer) provided by maropost.com
+    # @param api_key [String] is the auth token (String) that is validated on the server to authenticate the user
     def initialize(account = ENV["ACCOUNT"], api_key = ENV["API_KEY"])
       MaropostApi.instance_variable_set(:@api_key, api_key)
       MaropostApi.instance_variable_set(:@account, account)
     end
     
+    ##
+    # Gets the order for the specified id
+    # @param id [Integer] unique identifier of the order
     def get_order(id)
       full_path = full_resource_path("/find")
       query_params = MaropostApi.set_query_params({"where[id]" => id})
@@ -16,6 +24,9 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # gets the order for the specified original order id
+    # @param original_order_id [String] A unique identifier that's not changed since the creation date
     def get_order_for_original_order_id(original_order_id)
       full_path = full_resource_path("/#{original_order_id}")
       query_params = MaropostApi.set_query_params
@@ -23,6 +34,19 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # Creates a new order from the given parameters
+    # @param require_unique [Boolean] validates that the order has a unique original_order_id for the given contact.
+    # @param contact [Hash] Named parameter, which if exists, must contain :email as its key
+    # @param order [Hash] Named parameter, which must contain :order_date, :order_status, :original_order_id, :order_items as its keys
+    # @param @order_items [OrderItem] Custom Type containing key value pair to reference an order item
+    # @param add_tags [Array] List of string tags to be added in the new order
+    # @param remove_tags [Array] List of string tags to be removed from the new order
+    # @param uid [String] Unique Identifier
+    # @param list_ids [String|CSV] A csv list fo list ids
+    # @param grand_total [Float] An amount referencing the total of the order
+    # @param campaing_id [Integer] ID of the Campaign the order belongs to
+    # @param coupon_code [String] 
     def create_order(
       require_unique, 
       contact: {}, 
@@ -64,6 +88,10 @@ module MaropostApi
       MaropostApi.post_result(full_path, {"order" => params})
     end
     
+    ##
+    # updates an order fo the given original order id
+    # @param original_order_id [String] Orginal Order Id of the Order
+    # @param order [Hash] Key value pairs that must contain at least :order_date, :order_status and :order_items as its keys
     def update_order_for_original_order_id(original_order_id, order: {})
       raise ArgumentError.new('original_order_id is required') if original_order_id.nil?
       
@@ -81,6 +109,10 @@ module MaropostApi
       MaropostApi.put_result(full_path, form_body)   
     end
     
+    ##
+    # updates an order fo the given original order id
+    # @param original_order_id [String] Id of the Order
+    # @param order [Hash] Key value pairs that must contain at least :order_date, :order_status and :order_items as its keys
     def update_order_for_order_id(order_id, order: {})
       raise ArgumentError.new('order_id is required') if order_id.nil?
       
@@ -98,6 +130,9 @@ module MaropostApi
       MaropostApi.put_result(full_path, {order: order}, query_params)
     end
     
+    ##
+    # deletes an order fo the given original order id
+    # @param original_order_id [String] Orginal Order Id of the Order
     def delete_for_original_order_id(original_order_id)
       full_path = full_resource_path("/#{original_order_id}")
       query_params = MaropostApi.set_query_params
@@ -112,14 +147,22 @@ module MaropostApi
       MaropostApi.delete_result(full_path, query_params)
     end
     
+    ##
+    # deletes products for the given original order id
+    # @param original_order_id [String] Orginal Order Id of the Order
+    # @param product_ids [Array] List of product ids to delete
     def delete_products_for_original_order_id(original_order_id, product_ids)
-      raise TypeError.new('product_ids should be an Array') if ! product_ids.kind_of? Array
+      raise TypeError.new('product_ids should be an Array') until product_ids.is_a? Array
       full_path = full_resource_path("/#{original_order_id}")
       query_params = MaropostApi.set_query_params({"product_ids" => product_ids.join(',')})
       
       MaropostApi.delete_result(full_path, query_params)
     end
     
+    ##
+    # deletes products for the specified order id
+    # @param order_id [Integer] ID of the order
+    # @param product_ids [Array] List of product IDs to be deleted    
     def delete_products_for_order_id(order_id, product_ids)
       raise TypeError.new('product_ids should be an Array') if ! product_ids.kind_of? Array
       full_path = full_resource_path("/find")
--- lib/maropost_api/relational_tables.rb
+++ lib/maropost_api/relational_tables.rb
@@ -1,8 +1,15 @@
-module MaropostApi 
+module MaropostApi
+  ##
+  # Contains methods to operate various operations like create, update, delete or read
+  # from the Relational Table Api
   class RelationalTables
     
     attr_accessor :table_name
-    
+    ##
+    # Creates a new instance of Reports class.
+    # @param account [Integer] is authentic user account id (Integer) provided by maropost.com
+    # @param api_key [String] is the auth token (String) that is validated on the server to authenticate the user
+    # @param table_name [String] sets which relational table the service's operations should act against 
     def initialize(account = ENV["ACCOUNT"], api_key = ENV["API_KEY"], table_name:)
       MaropostApi.instance_variable_set(:@api_key, api_key)
       MaropostApi.instance_variable_set(:@account, account)
@@ -11,6 +18,8 @@ module MaropostApi
       MaropostApi.base_uri "https://rdb.maropost.com/#{account}"
     end
     
+    ##
+    # Gets the records of the relational table
     def get
       full_path = full_resource_path
       query_params = MaropostApi.set_query_params
@@ -18,6 +27,10 @@ module MaropostApi
       MaropostApi.get_result(full_path, query_params)
     end
     
+    ##
+    # Gets the specified record from the relational table
+    # @param unique_field_name [String] the name of the field to retrieve
+    # @param value [Mixed] Value of the unique_field_name
     def show(unique_field_name, value)
       raise TypeError.new("#{unique_field_name.inspect} should be a string") unless unique_field_name.is_a? String
       params = {:record => {}}
@@ -27,6 +40,10 @@ module MaropostApi
       MaropostApi.post_result(full_path, params)
     end
     
+    ##
+    # Adds a record to the Relational Table
+    # @param key_value_col [Hash] A key value pair to be added to the table
+    # @param key_value_cols [Hash] Other multile key value pairs that can be added at once in the table
     def create(key_value_col, *key_value_cols)
       raise TypeError.new("#{key_value_col.inspect} is not type of Hash") unless key_value_col.kind_of? Hash
       if (key_value_cols)
@@ -39,6 +56,10 @@ module MaropostApi
       MaropostApi.post_result(full_path, :record => all_key_values)
     end
     
+    ##
+    # Updates a record in the Relational Table
+    # @param key_value_col [Hash] A key value to be updated in the table
+    # @param key_value_cols [Hash] Rest of the key value pairs to be updated if there are multiple
     def update(key_value_col, *key_value_cols)
       raise TypeError.new("#{key_value_col.inspect} is not type of Hash") unless key_value_col.kind_of? Hash
       if (key_value_cols)
@@ -52,6 +73,10 @@ module MaropostApi
       MaropostApi.put_result(full_path, {:record => all_key_values}, query_params)
     end
     
+    ##
+    # Creates or updates a record in the Relational Table
+    # @param key_value_col [Hash] A key value to be updated inthe table
+    # @param key_value_cols [Hash] Rest of the key value pairs to be added/updated in case of multiple fields
     def upsert(key_value_col, *key_value_cols)
       raise TypeError.new("#{key_value_col.inspect} is not type of Hash") unless key_value_col.kind_of? Hash
       if (key_value_cols)
@@ -66,6 +91,10 @@ module MaropostApi
       MaropostApi.put_result(full_path, {:record => all_key_values}, query_params)
     end
     
+    ##
+    # Deletes the given record from the Relational Table
+    # @param unique_field_name [String] name of the unique identifier of the record to be deleted
+    # @param value [Mixed] value matching the unique identifier
     def delete(unique_field_name, value)
       raise TypeError.new("#{unique_field_name.inspect} is not a String") unless unique_field_name.is_a? String
       record = {:record => {}}
